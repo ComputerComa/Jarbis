58416524de6d50ff6335b7bb13da85fca78f35ce
ComputerComa <68386555+ComputerComa@users.noreply.github.com>
2021-12-30 04:18:42 -0600

    Update sotd.js

--- commands/sotd.js
+++ commands/sotd.js
@@ -41,7 +41,57 @@ function msToHms(time, ms) {
 
     return out
 }
-function buildSotdEmbed(ping_role,user_credit,spotify_url_to_parse,){}
+async function buildSotdEmbed(ping_role,user_credit,spotify_url_to_parse,){
+
+    const spotifydata = await getData(spotify_url_to_parse)
+        const album_image = spotifydata.album.images[1].url
+        const dominant_color = spotifydata.dominantColor
+        var explicit = spotifydata.explicit
+        if (explicit) {
+            explicit = "Yes"
+        } else {
+            explicit = "No"
+        }
+        const duration = spotifydata.duration_ms
+        var pretty_duration = msToHms(duration, ms)
+        const release_precision = spotifydata.album.release_date_precision
+        const date = spotifydata.album.release_date
+        if (release_precision == 'day') {
+            const date_split = date.split("-")
+            var dformatted = ""
+            dformatted = String(date_split[1]) + "/" + String(date_split[2] + "/" + String(date_split[0]))
+        } else if (release_precision == 'year') {
+            dformatted = spotifydata.album.release_date
+        }
+    const sotdPingEmbed = new MessageEmbed().setColor(dominant_color).setTitle("Announcement ping.").setDescription(`Hey ${ping_role}! There's a new SOTD suggestion!`).setAuthor("Johnny 5").setImage(album_image).addFields({
+        name: `Song`,
+        value: `${
+            spotifydata.name
+        }`
+    }, {
+        name: `Artist`,
+        value: `${
+            spotifydata.artists[0].name
+        }`
+    }, {
+        name: `Duration`,
+        value: `${pretty_duration}`
+    }, {
+        name: `Release ${release_precision}`,
+        value: `${dformatted}`
+    }, {
+        name: `Spotify URL`,
+        value: `${spotify_url_to_parse}`
+    }, {
+        name: `Explicit`,
+        value: `${explicit}`
+    }, {
+        name: `Suggested By:`,
+        value: `${user_credit}`
+    }).setFooter('Thanks for the song suggestion!')
+
+    return sotdPingEmbed
+}
 
 async function hasAnnouncedHistory(serverID, songID) {
     let history_count =  await SOTDHistory.count({guild_id: serverID.toString(), song_ID: songID.toString()})
@@ -58,11 +108,14 @@ module.exports = {
     async execute(interaction) {
       await interaction.deferReply({ephemeral: true})
         const spotify_url_to_parse = interaction.options.getString('spotify-url')
-        const song_ID = utils.getSongID(spotify_url_to_parse)
+        const songID = utils.getSongID(spotify_url_to_parse)
         const guild_ID = interaction.guild.id
-      let announced = await hasAnnouncedHistory(guild_ID,song_ID)
+        let sotdPingEmbed = await buildSotdEmbed(interaction.options.getRole('ping-role'),interaction.options.getUser('user-credit'),spotify_url_to_parse)
+      let announced= await hasAnnouncedHistory(guild_ID,songID)
       if(announced){
-        console.log("this song has been announced in this server before")
+        //console.log("this song has been announced in this server before")
+        let historyitem = await SOTDHistory.findOne({guild_id: guild_ID.toString(), song_ID: songID.toString()})
+        console.log(historyitem)
         const BtnYes = new MessageButton()
             .setCustomId('yes')
             .setLabel('Yes')
@@ -84,59 +137,33 @@ module.exports = {
             BtnYes,
             BtnNo
           )
-          interaction.editReply({ephemeral: true, embeds: [NoticeEmbed],components: [row]})
+          await interaction.editReply({ephemeral: true, embeds: [NoticeEmbed],components: [row]})
 
       }else{
         console.log("this song has not been announced in this server before")
-        const ping_role = interaction.options.getRole('ping-role')
-        const user_credit = interaction.options.getUser('user-credit');
-        const spotifydata = await getData(spotify_url_to_parse)
-        const album_image = spotifydata.album.images[1].url
-        const dominant_color = spotifydata.dominantColor
-        var explicit = spotifydata.explicit
-        if (explicit) {
-            explicit = "Yes"
-        } else {
-            explicit = "No"
-        }
-        const duration = spotifydata.duration_ms
-        var pretty_duration = msToHms(duration, ms)
-        const release_precision = spotifydata.album.release_date_precision
-        const date = spotifydata.album.release_date
-        if (release_precision == 'day') {
-            const date_split = date.split("-")
-            var dformatted = ""
-            dformatted = String(date_split[1]) + "/" + String(date_split[2] + "/" + String(date_split[0]))
-        } else if (release_precision == 'year') {
-            dformatted = spotifydata.album.release_date
-        }
-
-        
-
-        
-        await interaction.channel.send({embeds: [sotdPingEmbed]})
-
         var SOTDHistoryEntry = new SOTDHistory({guild_id: interaction.guild.id, song_ID: songID, date_announced: Date.now()})
+          SOTDHistoryEntry.save();
 
-        SOTDHistoryEntry.save();
-
-        interaction.reply({content: 'Announcement Sent!', ephemeral: true})
+       await interaction.reply({content: 'Announcement Sent!', ephemeral: true})
+        await interaction.channel.send({embeds: [sotdPingEmbed]})
       }
 
         
       const filter = i => i.user.id === interaction.user.id;
-      const collector = interaction.channel.createMessageComponentCollector({filter,max:1})
+      const collector = interaction.channel.createMessageComponentCollector({filter,time:15000})
 
       collector.on('collect',async i =>{
           if(i.customId === "yes"){
-              await i.update({embeds:[passable_embed],components: []})
+              await i.update({content: 'Announcement Sent!',ephemeral: true,embeds:[],components: []})
+              collector.stop()
+              await i.channel.send({embeds:[sotdPingEmbed]})
           }else if(i.customId === "no"){
               await i.update({ephemeral: true,content:"This announcement was canceled!",components:[],embeds:[]})
           }else{
               return
           }
+    
       })
-
     }
 
 };
